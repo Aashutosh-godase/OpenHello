b45d45d6056befbd3e50d4e76436e88a422845b0
Aashutosh-godase <aashutoshpgodase@gmail.com>
2025-09-17 12:59:48 +0530

    Update contributorsData.jsx

--- client/src/data/contributorsData.jsx
+++ client/src/data/contributorsData.jsx
@@ -1,8 +1,8 @@
 export const contributorsData = [
   {
-    name: "Siddesh Navthale",
+    name: "aashutosh godase",
     photoUrl: "https://avatars.githubusercontent.com/u/111569738?v=4",
-    githubUrl: "https://github.com/sidxhdev",
+    githubUrl: "https://github.com/Aashutosh-godase/aashu",
   },
   
  
